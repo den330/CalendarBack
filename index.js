a4e5ae625fedcbe16915bfa2ce7f0c235794a280
den330 <yaxinyuan0910@gmail.com>
2024-05-29 18:03:49 -0400

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
