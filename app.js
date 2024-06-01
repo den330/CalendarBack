fe4fa81e64b771396c8cd7e365f199744f6f2a60
den330 <yaxinyuan0910@gmail.com>
2024-06-01 00:12:27 -0400

    db setup

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const morgan = require("morgan");
 const signupRoute = require("./Routes/SignUpRoute");
 const loginRoute = require("./Routes/LoginRoute");
 const userRoute = require("./Routes/UserRoute");
 const calendarRoute = require("./Routes/CalendarRoute");
+
+mongoose.connect(`${process.env.dbUrl}`);
+const db = mongoose.connection;
 
 const app = express();
 
@@ -17,3 +22,7 @@ app.use("/user", userRoute);
 app.use("/calendar", calendarRoute);
 
 app.listen(3000);
+
+db.once("open", () => {
+  console.log("db connected");
+});
