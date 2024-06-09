ee664b623b73e9ae3f018459bf5cf3713e4ef9e1
den330 <yaxinyuan0910@gmail.com>
2024-06-09 18:58:49 -0400

    log

--- Configs/CorsOptions.js
+++ Configs/CorsOptions.js
@@ -3,6 +3,7 @@ const allowedOrigins = require("./AllowedOrigins");
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      console.log("Origins allowed: ", allowedOrigins);
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
